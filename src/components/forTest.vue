a00f29772c0de522cbc965852babd476604713b7
chiuaslan0620@gmail.com <chiuaslan0620@gmail.com>
2022-07-08 14:54:07 +0800

    for test add JS

--- src/components/forTest.vue
+++ src/components/forTest.vue
@@ -1,3 +1,26 @@
 <template>
-  <div class="test1"></div>
+  <div class="test1">
+    {{ msg }}
+  </div>
+  <div class="test2">
+    {{ testValue }}
+  </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: null,
+    },
+  },
+  setup (props) {
+    const testValue = ref(props.msg + 1)
+    return {
+      testValue,
+    }
+  },
+}
+</script>
